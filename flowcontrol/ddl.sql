8c6ca667efb37168abfb197c8e15ec3c7f54e36f
LevinLFX <levin@joycastle.mobi>
2022-07-21 21:12:06 +0800

    done

--- flowcontrol/ddl.sql
+++ flowcontrol/ddl.sql
@@ -6,21 +6,21 @@ CREATE TABLE `flow` (
   `add_time` int unsigned NOT NULL DEFAULT '0' COMMENT '添加时间',
   `update_time` int unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`id`),
-  KEY `idx_uniq_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='流量控制主表';
+  UNIQUE KEY `idx_uniq_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='流量控制主表';
 
 CREATE TABLE `flow_config` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
   `flow_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '流量控制ID',
   `strategy` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '策略名称',
   `value` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '控制值',
+  `extra` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '其他',
   `open` int unsigned NOT NULL DEFAULT '0' COMMENT '是否打开（0:关闭,1:打开）',
-  `priority` int unsigned NOT NULL DEFAULT '1' COMMENT '优先级',
   `add_time` int unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
   `update_time` int unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `flow_id` (`flow_id`,`priority`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='流量控制配置表';
+  UNIQUE KEY `idx_uniq_flow_id_strategy` (`flow_id`,`strategy`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='流量控制配置表';
 
 CREATE TABLE `flow_white_list` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
@@ -29,4 +29,4 @@ CREATE TABLE `flow_white_list` (
   `add_time` int unsigned DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_uniq_flow_id_sub_id` (`config_id`,`sub_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='流量控制白名单';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='流量控制白名单';
